3d61d037ea2b0efedd4035512409df650a972c48
vallari1 <111722727+vallari1@users.noreply.github.com>
2023-04-23 11:10:36 +0530

    fixed file path

--- DISTANCE-ESTIMATION/distance.py
+++ DISTANCE-ESTIMATION/distance.py
@@ -2,80 +2,63 @@ import cv2 as cv
 from cv2 import aruco
 import numpy as np
 
-calib_data_path = "../calib_data/MultiMatrix.npz"
+calibration_path = "./calibrate/MultiMatrix.npz"
+calibrate = np.load(calibration_path)
+print(calibrate.files)
+cam_mat = calibrate["camMatrix"]
+dist_coef = calibrate["distCoef"]
+r_vectors = calibrate["rVector"]
+t_vectors =calibrate["tVector"]
+MARKER_SIZE = 8  
+markerdicty = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-calib_data = np.load(calib_data_path)
-print(calib_data.files)
-
-cam_mat = calib_data["camMatrix"]
-dist_coef = calib_data["distCoef"]
-r_vectors = calib_data["rVector"]
-t_vectors = calib_data["tVector"]
-
-MARKER_SIZE = 8  # centimeters
-
-marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-
-param_markers = aruco.DetectorParameters_create()
-
-cap = cv.VideoCapture(0)
+parameterr = aruco.DetectorParameters_create()
+capture = cv.VideoCapture(0) #for camera
 
 while True:
-    ret, frame = cap.read()
+    ret, framee = capture.read()
     if not ret:
         break
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    marker_corners, marker_IDs, reject = aruco.detectMarkers(
-        gray_frame, marker_dict, parameters=param_markers
-    )
+    grey_frames = cv.cvtColor(framee, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(grey_frames, markerdicty, parameters=parameterr)
     if marker_corners:
         rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
-            marker_corners, MARKER_SIZE, cam_mat, dist_coef
-        )
+            marker_corners, MARKER_SIZE, cam_mat, dist_coef)
         total_markers = range(0, marker_IDs.size)
         for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
-            cv.polylines(
-                frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
-            )
+            cv.polylines(framee, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA)
             corners = corners.reshape(4, 2)
             corners = corners.astype(int)
             top_right = corners[0].ravel()
             top_left = corners[1].ravel()
             bottom_right = corners[2].ravel()
             bottom_left = corners[3].ravel()
-
-            # Since there was mistake in calculating the distance approach point-outed in the Video Tutorial's comment
-            # so I have rectified that mistake, I have test that out it increase the accuracy overall.
-            # Calculating the distance
             distance = np.sqrt(
-                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
-            )
-            # Draw the pose of the marker
-            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
+                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2)
+            
+            point = cv.drawFrameAxes(framee, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)#pose of markers will be given
             cv.putText(
-                frame,
+                framee,
                 f"id: {ids[0]} Dist: {round(distance, 2)}",
                 top_right,
                 cv.FONT_HERSHEY_PLAIN,
                 1.3,
                 (0, 0, 255),
                 2,
-                cv.LINE_AA,
-            )
+                cv.LINE_AA,)
             cv.putText(
-                frame,
+                framee,
                 f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
                 bottom_right,
                 cv.FONT_HERSHEY_PLAIN,
                 1.0,
                 (0, 0, 255),
                 2,
-                cv.LINE_AA,
-            )
-            # print(ids, "  ", corners)
-    cv.imshow("frame", frame)
-    key = cv.waitKey(1)
-    if key == ord("q"):
+                cv.LINE_AA,)
+            
+    cv.imshow("frame", framee)
+    exit_button = cv.waitKey(1)
+    if exit_button == ord("q"):
         break
-cap.release()
+capture.release()
 cv.destroyAllWindows()
